a3cff866590d7f3b1707c051e15f9d4906972fe1
mutkanz <m.gurinas@gmail.com>
2015-09-02 12:00:27 +0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 (function () {
 
     // Change this to your GitHub username so you don't have to modify so many things.
-    var fork = "Yemasthui";
+    var fork = "mutkanz";
 
     // Define our function responsible for extending the bot.
     function extend() {
@@ -37,6 +37,19 @@
                 }
               }
             };
+        bot.commands.baconCommand = {
+            command: 'auris',  // The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', // Minimum user permission to use the command
+            type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+              functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                  API.sendChat("/me Its C25: Mano draugužis 43cm ir gali apsivyniot aplink kaklą.");
+                }
+              }
+            };
+            
 
         // Load the chat package again to account for any changes
         bot.loadChat();
